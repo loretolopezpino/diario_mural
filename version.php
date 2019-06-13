2ec33beb9b3231a1a379c7b3f6fc8d098110cee1
Loreto  Maria Lopez Pino <loreto.lopez.p@uai.cl>
2019-06-13 16:36:56 -0400

    Version downgrade para instalación por primera vez.

--- version.php
+++ version.php
@@ -22,5 +22,5 @@
  */
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_diario_mural';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
-$plugin->version = 20190612302;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 20190612300;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2018051705 	; // YYYYMMDDHH (This is the release version for Moodle 3.5.5)
